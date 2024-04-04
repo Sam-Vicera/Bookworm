de9900892d2fd1a1516c629c7332e0f6a084953d
Samuel Hernandez Vicera <shernandezvicera@apple.com>
2024-04-04 11:35:03 -0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Samuel Hernandez Vicera on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
